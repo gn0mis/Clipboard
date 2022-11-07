0fc404dfc4456721df099b3e5e586ae8102fcfab
simonbjerkas <simon.bjerkas10@gmail.com>
2022-11-08 00:32:16 +0100

    Adding writer class and generalising reader

--- src/reader.py
+++ src/reader.py
@@ -4,13 +4,18 @@ import yaml
 class Reader:
     
     def __init__(self, filepath) -> None:
-        self.commands = {}
+        self.out = {}
         if os.path.exists(filepath):
             with open(filepath, "r") as stream:
                 try:
                     for line in stream:
                         words =line.split()
-                        com = words[0][:-1]
-                        self.commands[com] = line.split(" ", 1)[1][:-1]
+                        arg = words[0][:-1]
+                        self.out[arg] = line.split(" ", 1)[1]
                 except yaml.YAMLError as e:
                     print(f"Reading of file failed: {e}")
+
+
+file = os.path.dirname(os.path.dirname(__file__)) + "/history/test.json"
+r = Reader(file)
+print(r.out)
---
+++ src/writer.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+class Writer:
+
+    def __init__(self, filename, data=dict) -> None:
+        path = os.path.dirname(os.path.dirname(__file__)) + "/history"
+        filepath = os.path.join(path, filename)
+
+        with open(filepath, "w") as f:
+            json.dump(data, f)
+
+t = Writer("test.json", {"key": "value"})
