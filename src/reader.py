63d8e7958995b2cbbf6280ebc39f81f98facf70f
simonbjerkas <simon.bjerkas10@gmail.com>
2022-11-07 23:45:32 +0100

    Adding reader class and config file

---
+++ src/reader.py
@@ -0,0 +1,16 @@
+import os
+import yaml
+
+class Reader:
+    
+    def __init__(self, filepath):
+        self.commands = {}
+        if os.path.exists(filepath):
+            with open(filepath, "r") as stream:
+                try:
+                    for line in stream:
+                        words =line.split()
+                        com = words[0][:-1]
+                        self.commands[com] = line.split(" ", 1)[1][:-1]
+                except yaml.YAMLError as e:
+                    print(f"Reading of file failed: {e}")
