92753ca7534c7bf6ef171dabc3cea69db3c623aa
simonbjerkas <simon.bjerkas10@gmail.com>
2022-11-08 00:39:28 +0100

    Updateing reader and writer with json capabilities

--- src/reader.py
+++ src/reader.py
@@ -1,21 +1,22 @@
 import os
 import yaml
+import json
 
 class Reader:
     
-    def __init__(self, filepath) -> None:
+    def __init__(self, filepath=str) -> None:
         self.out = {}
         if os.path.exists(filepath):
-            with open(filepath, "r") as stream:
-                try:
-                    for line in stream:
-                        words =line.split()
-                        arg = words[0][:-1]
-                        self.out[arg] = line.split(" ", 1)[1]
-                except yaml.YAMLError as e:
-                    print(f"Reading of file failed: {e}")
+            if filepath.endswith(".json"):
+                with open(filepath, "r") as stream:
+                    self.out = json.load(stream)
+            else:
+                with open(filepath, "r") as stream:
+                    try:
+                        for line in stream:
+                            words =line.split()
+                            arg = words[0][:-1]
+                            self.out[arg] = line.split(" ", 1)[1][:-1]
+                    except yaml.YAMLError as e:
+                        print(f"Reading of file failed: {e}")
 
-
-file = os.path.dirname(os.path.dirname(__file__)) + "/history/test.json"
-r = Reader(file)
-print(r.out)
--- src/writer.py
+++ src/writer.py
@@ -3,11 +3,9 @@ import os
 
 class Writer:
 
-    def __init__(self, filename, data=dict) -> None:
+    def __init__(self, filename=str, data=dict) -> None:
         path = os.path.dirname(os.path.dirname(__file__)) + "/history"
         filepath = os.path.join(path, filename)
 
         with open(filepath, "w") as f:
             json.dump(data, f)
-
-t = Writer("test.json", {"key": "value"})
