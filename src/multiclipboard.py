38321956e93b34394f70eb88aa9d139f7031ec9f
simonbjerkas <simon.bjerkas10@gmail.com>
2022-11-08 01:14:40 +0100

    Save command execution

--- src/multiclipboard.py
+++ src/multiclipboard.py
@@ -1,30 +1,39 @@
-import sys
 import clipboard
 import json
 import os
+from sys import argv
+from datetime import date
 
 import reader
+import writer
 
 PARENT = os.path.dirname(os.path.dirname(__file__))
 
 com_file = os.path.join(PARENT, "commands.yaml")
-commands = reader.Reader(com_file).commands
+commands = reader.Reader(com_file).out
 command_list = list(commands.keys())
     
-if len(sys.argv) == 2:
-    command = sys.argv[1]
-            
-    if command == command_list[0]:
+if len(argv) <= 3:
+    command = argv[1]
+    filename = str(date.today()) + ".json"
+    data = reader.Reader(PARENT + "/history/" + filename).out
+    
+    if command == "save" and len(argv) == 3:
+        key = argv[2]
+        value = clipboard.paste()
+        data[key] = value
+        writer.Writer(filename, data)
+
+    elif command == "load" and len(argv) == 2:
         print(command)
-    elif command == command_list[1]:
+    elif command == "list" and len(argv) == 2:
         print(command)
-    elif command == command_list[2]:
-        print(command)
-    elif command == command_list[3]:
+    elif command == "--help" and len(argv) == 2:
         print("The different commands are: ")
         for command in command_list:
             print("  " + command + ": " + commands[command])
     else:
         print("Unknown command. Type '--help' for more information.")
 else:
-    print("Please pass exactly one command. For more info write '--help'.")
+    print("If using 'save' command, pass save command and one string "
+    "being used as a key. Otherwise, pass exactly one command  For more info write '--help'.")
