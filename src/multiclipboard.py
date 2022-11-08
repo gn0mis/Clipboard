1975ff0a1feb6e9fa600993d4d521b71b8b8c970
simonbjerkas <simon.bjerkas10@gmail.com>
2022-11-08 02:01:57 +0100

    Load command execution

--- src/multiclipboard.py
+++ src/multiclipboard.py
@@ -1,13 +1,15 @@
 import clipboard
 import json
 import os
+import re
 from sys import argv
-from datetime import date
+from datetime import date, datetime
 
 import reader
 import writer
 
 PARENT = os.path.dirname(os.path.dirname(__file__))
+DATE_CHECK = "^[0-9]{4}\\-[0-9]{1,2}\\-[0-9]{1,2}$"
 
 com_file = os.path.join(PARENT, "commands.yaml")
 commands = reader.Reader(com_file).out
@@ -25,8 +27,19 @@ if len(argv) <= 3:
         writer.Writer(filename, data)
         print("Data saved")
 
-    elif command == "load" and len(argv) == 2:
-        print(command)
+    elif command == "load" and len(argv) == 3:
+        key = argv[2]
+
+        if re.match(DATE_CHECK, key):
+            r = reader.Reader(PARENT + "/history/" + key + ".json").out
+            for key in r:
+                print(f"{key}: {r[key]}")
+
+        elif data.get(key):
+            print(data.get(key))
+        else:
+            print("Given key hasn't been logged today.")
+
     elif command == "list" and len(argv) == 2:
         print(command)
     elif command == "--help" and len(argv) == 2:
